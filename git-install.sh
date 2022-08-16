3f60c379d260565d9859132fd574677284c84660
Mason Choat <102649685+Mvcvalli@users.noreply.github.com>
2022-08-16 07:32:23 +0000

    Add files via upload

---
+++ git-install.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+sudo -v
+
+echo ' '
+echo "This script will install Mason Choat's dotfiles."
+echo "Make sure to adapt this script to your needs and preferences before using it."
+echo "An internet connection is needed to use this script."
+echo ' '
+echo "Press [ENTER] key when ready to continue"
+read y
+
+echo ''
+echo "Starting install..."
+echo ''
+
+# Moving to home directory.
+cd ~
+
+# Installing dotfiles.
+mkdir Repo
+cd Repo
+git clone --depth 1 https://github.com/Mvcvalli/dotfiles.git
+
+# Installing nvim config.
+cd ~/Repo/dotfiles/.config
+git clone --depth 1 https://github.com/Mvcvalli/nvim.git
+cd ~
+
+# Removing unnecessary files
+cd ~/Repo/dotfiles/.config/nvim
+rm -rf .git
+cd ~
+rm -rf .config # One could exist, idk.
+
+# Moving shit around
+mv ~/Repo/dotfiles/.config ~/
+mv ~/Repo/dotfiles/.newsboat ~/
+mv ~/Repo/dotfiles/.vimrc ~/
+
+# Installing vim-plug (Vim)
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Installing vim-plug (Neovim)
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+cd ~
+rm -rf Repo
+
+echo ''
+echo "Install complete"
