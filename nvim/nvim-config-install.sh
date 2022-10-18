a8797b53dab32c80523cc8e8e33cd3d22bc52c94
Mason Choat <102649685+Mvcvalli@users.noreply.github.com>
2022-10-18 07:28:18 +0000

    Create nvim-config-install.sh
    A dumb script that probably shouldn't exist.

---
+++ nvim/nvim-config-install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo -v # Give sudo privileges ahead of time, so that we hopefully only have to enter a password once.
+cd ~    # Moving to home directory at the beginning of the process.
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+ 
+cd ~/.config
+git clone --depth 1 https://github.com/Mvcvalli/nvim
+cd ~
