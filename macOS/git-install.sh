de34609831bd0edd66e4a071eb39340e522519f6
Mason Choat <102649685+Mvcvalli@users.noreply.github.com>
2022-10-19 04:31:28 +0000

    Update git-install.sh

--- macOS/git-install.sh
+++ macOS/git-install.sh
@@ -4,50 +4,47 @@ echo ' '
 echo "Make sure to adapt this script to your needs and preferences before using it."
 echo "An internet connection is needed to use this script."
 echo ' '
-echo "Press [ENTER] key when ready to continue"
+echo "Press [ENTER] key when ready to continue."
 read y
 
-echo ''
-echo "Starting install..."
-echo ''
+sudo -v  # Give sudo privileges ahead of time, so that we hopefully only have to enter a password once.
+cd $HOME # Moving to home directory at the beginning of the process.
 
-sudo -v # Give sudo privileges ahead of time, so that we hopefully only have to enter a password once.
-cd ~    # Moving to home directory at the beginning of the process.
+# Removing unnecessary files.
+rm -rf .config # One could exist, idk.
+rm -rf .local  # Again, one could exist, idk.
+rm -rf .newsboat
+rm -rf .vim
+cd $HOME
 
 # Installing dotfiles.
 mkdir Repo
 cd Repo
 git clone --depth 1 https://github.com/Mvcvalli/dotfiles.git
-cd ~
+cd $HOME
 
-# Removing unnecessary files
-rm -rf .config # One could exist, idk.
-rm -rf .local  # Again, one could exist, idk.
-
-# Moving shit around
+# Moving shit around.
 mv ~/Repo/dotfiles/.config ~/
 mv ~/Repo/dotfiles/.newsboat ~/
 mv ~/Repo/dotfiles/.vim ~/
-cd~
-
-# Installing vim-plug (Vim)
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-       
-# Installing Vim plugins.
-vim -c "PlugInstall|q|q"
+cd $HOME
 
 # Installing scripts.
-cd ~/.local
+mkdir .local
+cd .local
 git clone --depth 1 https://github.com/Mvcvalli/scripts.git
 cd scripts
 rm -rf .git linux zsh
-cd macOS chmod +x git-install.sh update.sh
-cd .. & cd nvim
+cd macOS 
+chmod +x git-install.sh update.sh
+cd ..  
+cd nvim
 chmod +x nvim-config-install.sh
+cd $HOME
 
-cd ~
-rm -rf Repo
+# Installing vim-plug.
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo ''
-echo "Install complete"
+# Updating Vim plugins.
+vim -c "PlugUpdate|q|q"
