6eddb6caf2ac6b346a1666f131a33ac3d479ccef
Mason Choat <102649685+Mvcvalli@users.noreply.github.com>
2022-10-22 02:13:25 +0000

    Update nvim-config-install.sh

--- nvim/nvim-config-install.sh
+++ nvim/nvim-config-install.sh
@@ -3,6 +3,8 @@
 sudo -v # Give sudo privileges ahead of time, so that we hopefully only have to enter a password once.
 cd ~    # Moving to home directory at the beginning of the process.
 
+brew install nvim
+
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
  
