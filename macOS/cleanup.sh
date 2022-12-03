182b8164773992f09c08b195a3f7bfc5ac0f1ea4
Mason <102649685+Mvcvalli@users.noreply.github.com>
2022-12-03 00:13:20 +0000

    Add files via upload

---
+++ macOS/cleanup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive sudo until `clenaup.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+oldAvailable=$(df / | tail -1 | awk '{print $4}')
+
+echo ''
+echo 'Empty the Trash on all mounted volumes and the main HDD...'
+sudo rm -rfv /Volumes/*/.Trashes/* &>/dev/null
+sudo rm -rfv ~/.Trash/* &>/dev/null
+
+echo ''
+echo 'Clear System Log Files...'
+sudo rm -rfv /private/var/log/asl/*.asl &>/dev/null
+sudo rm -rfv /Library/Logs/DiagnosticReports/* &>/dev/null
+sudo rm -rfv /Library/Logs/Adobe/* &>/dev/null
+rm -rfv ~/Library/Containers/com.apple.mail/Data/Library/Logs/Mail/* &>/dev/null
+rm -rfv ~/Library/Logs/CoreSimulator/* &>/dev/null
+
+echo ' '
+echo "Cleaning up LaunchServices to remove duplicates in the “Open With” menu"
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder
+
+echo ' '
+echo "Recursively deleting `.DS_Store` files..."
+sudo find . -type f -name '*.DS_Store' -ls -delete
+
+echo ' '
+echo "Cleaning caches & directories..."
+brew cleanup -s
+brew -v cleanup --prune=2
+rm -rf "$(brew --cache)"
+
+echo ' '
+echo "All done!"
