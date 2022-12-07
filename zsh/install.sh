750c5698dabf2f06335401ef16a3c9ec810445da
Mason <102649685+Mvcvalli@users.noreply.github.com>
2022-12-07 08:49:12 +0000

    Update install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,3 +6,5 @@ sh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.sh)
 # Installing ZSH config. 
 cd ~/.config
 git clone --depth 1 https://github.com/Mvcvalli/zsh.git
+cd zsh
+mv .zshenv ~/
