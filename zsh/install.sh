e6b6d0e5d0f381680f3e6dd40028dcb44422784c
Mason <102649685+Mvcvalli@users.noreply.github.com>
2022-12-07 08:41:45 +0000

    Update and rename zap-install.sh to install.sh

---
+++ zsh/install.sh
@@ -0,0 +1,7 @@
+
+# Plugin Manager (Zap)
+sh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.sh)
+
+# Installing ZSH config. 
+cd ~/.config
+git clone --depth 1 https://github.com/Mvcvalli/zsh.git
--- zsh/zap-install.sh
+++
@@ -1,1 +0,0 @@
-sh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.sh)
