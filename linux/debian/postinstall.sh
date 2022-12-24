9daa29e4320bcdf9cc27804544b6888917213c5e
Mason <102649685+Mvcvalli@users.noreply.github.com>
2022-12-25 00:01:01 +1300

    Update postinstall.sh

--- linux/debian/postinstall.sh
+++ linux/debian/postinstall.sh
@@ -35,6 +35,25 @@ mv alacritty mpv vim zsh ~/.config
 # Scripts Install
 cd ~/.local
 git clone --depth 1 https://github.com/Mvcvalli/scripts.git
+cd scripts
+rm -rf .git
+cd linux/arch
+chmod +x arch-bootstrap.sh arch-cleanup.sh arch-update.sh
+cd ..
+cd debian
+chmod +x postinstall.sh
+cd ..
+cd ..
+cd macOS
+chmod +x bootstrap.sh cleanup.sh git-install.sh update.sh
+cd ..
+cd nvim
+chmod +x nvim-config-install.sh uninstall-nvim.sh
+cd ..
+cd zsh
+chmod +x install.sh
+cd ..
+chmod +x script-install-script.sh
 
 # ZSH Plugin Manager Install
 sh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.sh)
