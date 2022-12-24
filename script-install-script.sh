2fb0e38f46cc192779bc582a912515877efa3d03
Mason <102649685+Mvcvalli@users.noreply.github.com>
2022-12-24 23:55:22 +1300

    Add files via upload

---
+++ script-install-script.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+cd ~/.local
+
+git clone https://github.com/Mvcvalli/scripts.git
+
+cd scripts
+
+rm -rf .git
+
+cd linux/arch
+
+chmod +x arch-bootstrap.sh arch-cleanup.sh arch-update.sh
+
+cd ..
+
+cd debian
+
+chmod +x postinstall.sh
+
+cd ..
+
+cd ..
+
+cd macOS
+
+chmod +x bootstrap.sh cleanup.sh git-install.sh update.sh
+
+cd ..
+
+cd nvim
+
+chmod +x nvim-config-install.sh uninstall-nvim.sh
+
+cd ..
+
+cd zsh
+
+chmod +x install.sh
